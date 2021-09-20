b2dca272c87019c71021d41a6eba4c6e8e240df5
Biswajit <biswajit.majumdar@carnationit.com>
2021-09-20 16:18:29 +0530

    My first commit for assesment1

---
+++ src/Components/Assesment1.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Style from './Style.css'
+import logo from './logo.png';
+
+class Assesment1 extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div className = "BackGround" >
+                <div className = "Logo">
+                    <div className ="child">
+                        <img src ={logo} alt = "logo of care.com"/>
+                    </div>
+                    <div className = "TopManue">
+                        <div className ="child">contact us |   </div>
+                        <div className ="child">
+                        <select name ="country" id="counrty">
+                            <option value = "country">Change country</option>
+                        </select>
+                        </div>
+                        <div className ="child">
+                            <div className = "ContainerLogIn">
+                                <button type="button">login</button>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+                hi
+            </div>
+        )
+    }
+}
+
+export default Assesment1
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Assesment1 from './Components/Assesment1'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Assesment1 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
